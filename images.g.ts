2f485977a20bc876944dc5e5f525878aff37f194
Mariogamer1000 <168658501+Mariogamer1000@users.noreply.github.com>
2024-09-16 19:57:11 -0400

    new areas! also suports gamecube controler!

--- images.g.ts
+++ images.g.ts
@@ -60,44 +60,6 @@ c . . f 8 8 8 8 8 f f 2 2 f . .
 . . . . f f f f f f . f f . . . 
 . . . e e e . . e e . . . . . . 
 `;
-            case "image2":
-            case "myImage0":return img`
-. . f 2 2 2 f f f f 2 2 2 f . . 
-. f 2 2 f f f 7 7 f f f 2 f . . 
-f 2 2 f f f 7 7 7 7 f f f 2 f . 
-f 2 f f f 7 7 7 7 7 7 f f f f f 
-f 2 f f 7 7 7 7 7 7 7 7 7 f 2 f 
-f 2 f 7 7 f f f f f f 7 7 f 2 f 
-f 2 f f f f d d d d f f f f 2 f 
-. f f e f 1 1 d d 1 1 f e f f . 
-. f e e d 1 8 d d 8 1 d e e f . 
-. . f e e d d d d d d e e f . . 
-. . . f e e d d d d e e f . . . 
-. . 1 1 7 7 7 7 7 7 7 f . . . . 
-. . b b f 5 8 8 8 8 5 f . . . . 
-. . b b f 8 8 8 8 8 8 f . . . . 
-. c b b c f f f f f f . . . . . 
-. . . . . e e . . e e . . . . . 
-`;
-            case "image5":
-            case "myImage3":return img`
-. . f 2 2 2 f f f f 2 2 2 f . . 
-. f 2 2 f f f 7 7 f f f 2 f . . 
-f 2 2 f f f 7 7 7 7 f f f 2 f . 
-f 2 f f f 7 7 7 7 7 7 f f f f f 
-f 2 f f 7 7 7 7 7 7 7 7 7 f 2 f 
-f 2 f 7 7 f f f f f f 7 7 f 2 f 
-f 2 f f f f d d d d f f f f 2 f 
-. f f e f 1 1 d d 1 1 f e f f . 
-. f e e d 1 8 d d 8 1 d e e f . 
-. . f e e d d d d d d e e f . . 
-. . . f e e d d d d e e f . . . 
-. . 1 1 7 7 7 7 7 7 7 f . . . . 
-. . b b f 5 8 8 8 8 5 f . . . . 
-. . b b f 8 8 8 8 8 8 f . . . . 
-. c b b c f f f f f f . . . . . 
-. . . . . e e . . e e . . . . . 
-`;
             case "image7":
             case "myImage5":return img`
 ....................ffff......
@@ -135,6 +97,155 @@ f 2 f f f f d d d d f f f f 2 f
 ....f888888f22f...............
 .....ffffff.ff................
 .....ee..ee...................
+`;
+            case "image8":
+            case "myImage6":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . 2 2 . . . . . 2 2 . . . . . 
+. 2 2 2 2 2 2 2 2 2 2 2 . . . . 
+. 2 2 2 2 2 2 2 2 2 2 2 2 . . . 
+. 2 2 2 2 2 2 2 2 2 2 2 2 . . . 
+. 2 2 2 2 2 2 2 2 2 2 2 2 . . . 
+. 2 2 2 2 2 2 2 2 2 2 2 2 . . . 
+. 2 2 2 2 2 2 2 2 2 2 2 2 . . . 
+. 2 2 2 2 2 2 2 2 2 2 2 2 . . . 
+. 2 2 2 2 2 2 2 2 2 2 2 2 . . . 
+. . 2 2 2 2 2 2 2 2 2 2 . . . . 
+. . . 2 2 2 2 2 2 2 2 . . . . . 
+`;
+            case "image9":
+            case "myImage7":return img`
+aaaaaaaaaaccccccccccccaaaaaaaaaa
+aaaaaaaaccc7777777777cccaaaaaaaa
+aaaaaaccc77777777777777cccaaaaaa
+aaaaacc777777777777777777ccaaaaa
+aaaacc77777777777777777777ccaaaa
+aaaac777777777fff7777777777caaaa
+aaacc77777777fffff777777777ccaaa
+aaacc7777777ffffff777777777ccaaa
+aaacc7777777fff7fff77777777ccaaa
+aaacc777777fffffffff7777777ccaaa
+aaacc77777ffffffffff7777777ccaaa
+aaacc7777ffff77117fff777777ccaaa
+aaacc7777fff777117ffff77777ccaaa
+aaacc77777ff7771177fff77777ccaaa
+aaaccc77777777711777777777cccaaa
+aaaccccc7777711111177777cccccaaa
+aaaacccccc777111111777ccccccaaaa
+aaaaccccccccc111111cccccccccaaaa
+aaaaacccccccc111111ccccccccaaaaa
+aaaaaacccccccccc7cccccccccaaaaaa
+aaaaaaaacccccccc7cccccccaaaaaaaa
+aaaaaaaaaacccccc7cccccaaaaaaaaaa
+aaaaaaaaaaaaaaaa7aaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaa7aaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaa7aaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaa7aaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaa7aaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaa7aaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaa7aaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaa7aaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaa7aaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaa7aaaaaaaaaaaaaaa
+`;
+            case "image10":
+            case "myImage8":return img`
+. . . . f f f f f f . . . . . . 
+. . . f 7 7 7 7 7 7 f f . . . . 
+. . f 7 7 7 7 7 7 7 1 f f . . . 
+. . f 7 7 7 7 7 7 1 7 1 f . . . 
+. f 7 7 7 7 7 7 7 1 7 1 f . . . 
+. f 7 7 f f f f 1 1 7 7 1 f . . 
+. f f f e e e f f f f f f f . . 
+. f e e d d d d d 1 1 d f f . . 
+. . f e e d d d d 1 8 d f f . . 
+. f f f e e d d d d d d f . . . 
+f 2 2 f e e d d d d f f . . . c 
+f 2 2 f 7 7 7 7 7 7 7 1 1 b b b 
+f 2 2 f 8 8 5 8 5 8 f 1 1 b b b 
+f 2 2 f 8 8 8 8 8 f f . . . . c 
+. f f . f f f f f f . . . . . . 
+8 9 8 8 9 8 8 9 8 9 8 9 8 9 8 9 
+`;
+            case "image11":
+            case "myImage9":return img`
+. . . . f f f f f f . . . . . . 
+. . . f 7 7 7 7 7 7 f f . . . . 
+. . f 7 7 7 7 7 7 7 7 f f . . . 
+. . f 1 7 7 7 7 7 7 7 7 f . . . 
+. f 1 7 1 7 7 7 7 7 7 7 f . . . 
+. f 7 7 1 7 7 7 7 7 7 7 7 f . . 
+. f f f f f f f f f f f f f . . 
+. f 1 1 d d d d d d d e f f . . 
+. f 8 1 d d d d d d e e f f . . 
+. f d d d d d d d e e e f . . . 
+c . f f d d d d d e f f f . . . 
+b b 1 1 7 7 7 7 7 7 f 2 2 f . . 
+b b 1 1 5 8 5 8 8 8 f 2 2 f . . 
+c . . f 8 8 8 8 8 f f 2 2 f . . 
+. . . . f f f f f f . f f . . . 
+9 8 9 8 9 8 9 8 9 8 8 9 8 9 8 9 
+`;
+            case "image12":
+            case "myImage10":return img`
+9 8 9 8 9 8 9 8 9 8 9 8 8 9 8 9 
+. . . . . f f f f f f c b b c . 
+. . . . f 8 8 8 8 8 8 f b b . . 
+. . . . f 5 8 8 8 8 5 f b b . . 
+. . . . f 7 7 7 7 7 7 7 1 1 . . 
+. . . f e e 4 4 4 4 e e f . . . 
+. . f e e d d d d d d e e f . . 
+. f e e d 1 8 d d 8 1 4 e e f . 
+. f f e f 1 1 d d 1 1 f e f f . 
+f 2 f f f f d d d d f f f f 2 f 
+f 2 f 7 7 f f f f f f 7 7 f 2 f 
+f 2 f 7 7 7 7 7 7 7 7 7 f f 2 f 
+. f f f f 7 7 7 7 7 7 f f f 2 f 
+. f 2 f f f 7 7 7 7 f f f 2 2 f 
+. . f 2 f f f 7 7 f f f 2 2 f . 
+. . f 2 2 2 f f f f 2 2 2 f . . 
+`;
+            case "image2":
+            case "myImage0":return img`
+. . f 2 2 2 f f f f 2 2 2 f . . 
+. f 2 2 f f f 7 7 f f f 2 f . . 
+f 2 2 f f f 7 7 7 7 f f f 2 f . 
+f 2 f f f 7 7 7 7 7 7 f f f f f 
+f 2 f f 7 7 7 7 7 7 7 7 7 f 2 f 
+f 2 f 7 7 f f f f f f 7 7 f 2 f 
+f 2 f f f f d d d d f f f f 2 f 
+. f f e f 1 1 d d 1 1 f e f f . 
+. f e e d 1 8 d d 8 1 d e e f . 
+. . f e e d d d d d d e e f . . 
+. . . f e e d d d d e e f . . . 
+. . 1 1 7 7 7 7 7 7 7 f . . . . 
+. . b b f 5 8 8 8 8 5 f . . . . 
+. . b b f 8 8 8 8 8 8 f . . . . 
+. c b b c f f f f f f . . . . . 
+. . . . . e e . . e e . . . . . 
+`;
+            case "image5":
+            case "myImage3":return img`
+. . f 2 2 2 f f f f 2 2 2 f . . 
+. f 2 2 f f f 7 7 f f f 2 f . . 
+f 2 2 f f f 7 7 7 7 f f f 2 f . 
+f 2 f f f 7 7 7 7 7 7 f f f f f 
+f 2 f f 7 7 7 7 7 7 7 7 7 f 2 f 
+f 2 f 7 7 f f f f f f 7 7 f 2 f 
+f 2 f f f f d d d d f f f f 2 f 
+. f f e f 1 1 d d 1 1 f e f f . 
+. f e e d 1 8 d d 8 1 d e e f . 
+. . f e e d d d d d d e e f . . 
+. . . f e e d d d d e e f . . . 
+. . 1 1 7 7 7 7 7 7 7 f . . . . 
+. . b b f 5 8 8 8 8 5 f . . . . 
+. . b b f 8 8 8 8 8 8 f . . . . 
+. c b b c f f f f f f . . . . . 
+8 9 8 9 8 9 8 9 8 9 8 9 8 9 8 9 
 `;
         }
         return null;
--- main.ts
+++ main.ts
@@ -7,11 +7,29 @@ namespace SpriteKind {
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.setImage(assets.image`myImage1`)
 })
+scene.onOverlapTile(SpriteKind.Player, sprites.swamp.swampTile1, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`level0`)
+    tiles.placeOnRandomTile(mySprite, assets.tile`tile12`)
+})
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.setImage(assets.image`myImage2`)
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.setImage(assets.image`myImage`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile17`, function (sprite, location) {
+    if (controller.right.isPressed()) {
+        mySprite.setImage(assets.image`myImage8`)
+    }
+    if (controller.left.isPressed()) {
+        mySprite.setImage(assets.image`myImage9`)
+    }
+    if (controller.up.isPressed()) {
+        mySprite.setImage(assets.image`myImage10`)
+    }
+    if (controller.down.isPressed()) {
+        mySprite.setImage(assets.image`myImage3`)
+    }
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.setImage(assets.image`myImage0`)
@@ -19,19 +37,31 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
 scene.onOverlapTile(SpriteKind.Player, assets.tile`transparency16`, function (sprite, location) {
     tiles.setCurrentTilemap(tilemap`level`)
 })
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairLarge, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`level_3`)
+    tiles.placeOnRandomTile(mySprite, assets.tile`myTile4`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile11`, function (sprite, location) {
+    if (controller.A.isPressed()) {
+        tiles.setCurrentTilemap(tilemap`level_0`)
+        tiles.placeOnRandomTile(mySprite, assets.tile`myTile14`)
+    }
+})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     if (controller.A.isPressed()) {
         sprites.destroy(myEnemy)
         info.changeScoreBy(1)
         music.play(music.createSoundEffect(WaveShape.Sine, 1, 5000, 255, 0, 150, SoundExpressionEffect.Warble, InterpolationCurve.Curve), music.PlaybackMode.InBackground)
+        info.setLife(100)
     }
 })
 let myEnemy: Sprite = null
 let mySprite: Sprite = null
 info.setLife(100)
 mySprite = sprites.create(assets.image`myImage0`, SpriteKind.Player)
+pause(100)
 controller.moveSprite(mySprite)
-tiles.setTilemap(tilemap`level_0`)
+tiles.setTilemap(tilemap`level_3`)
 scene.cameraFollowSprite(mySprite)
 myEnemy = sprites.create(img`
     . . . . . . . . . . . . . . . . 
